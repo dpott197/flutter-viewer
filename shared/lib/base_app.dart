aca6b17568fea289fb4882c54f62f4299838ba0e
Darren Pottinger <darren.pottinger@gmail.com>
2023-10-21 15:21:31 -0400

    Sets fallback widget

--- shared/lib/base_app.dart
+++ shared/lib/base_app.dart
@@ -112,27 +112,54 @@ class DetailView extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          character.iconUrl != null
-              ? ClipOval(
-            child: Image.network(
-              character.iconUrl!,
-              width: 100,
-              height: 100,
-              fit: BoxFit.cover,
+          Center(
+            child: ClipOval(
+              child: Image.network(
+                character.iconUrl ?? '', // FIXME
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    radius: 50,
+                    child: Text(
+                      _getInitials(character.name),
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                  );
+                },
+              ),
             ),
-          )
-              : Container(),
-          SizedBox(height: 20),
-          Text(character.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-          SizedBox(height: 20),
+          ),
+          SizedBox(height: 16),
+          Text(character.name,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          SizedBox(height: 8),
           Text(character.description),
         ],
       ),
     );
   }
+
+  String _getInitials(String name) {
+    List<String> names = name.split(' ');
+    String initials = '';
+    int numWords = 2;
+
+    for (var i = 0; i < names.length && i < numWords; i++) {
+      if (names[i].isNotEmpty) {
+        initials += names[i][0].toUpperCase();
+      }
+    }
+
+    return initials;
+  }
 }
+
 
 class DetailScreen extends StatelessWidget {
   final Character character;
