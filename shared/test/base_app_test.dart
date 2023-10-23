b8288244f49bfd4f46c45a45d7b512b17c60cacc
Darren Pottinger <darren.pottinger@gmail.com>
2023-10-23 06:33:59 -0400

    Updates test description

--- shared/test/base_app_test.dart
+++ shared/test/base_app_test.dart
@@ -1,9 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared/base_app.dart';
 
-
 void main() {
-  test('adds one to input values', () {
+  test('identifies handset based on width', () {
     expect(DeviceType.isHandset(601), false);
     expect(DeviceType.isHandset(600), false);
     expect(DeviceType.isHandset(599), true);
